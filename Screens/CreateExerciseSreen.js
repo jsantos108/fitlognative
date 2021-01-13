d3100a3ce04e47207cc8e38d317f546ce92f67de
Jose Santos <jsantos108@gmail.com>
2021-01-13 00:32:46 -0800

    Started Create Exercise User Selector

--- Screens/CreateExerciseSreen.js
+++ Screens/CreateExerciseSreen.js
@@ -1,10 +1,39 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import axios from 'axios';
 import {View, Text} from 'react-native';
+import {Picker} from '@react-native-picker/picker';
 
 function CreateExerciseScreen({ navigation }) {
+
+    // const {users, setUsers} = useState([]);
+    const [selectedValue, setSelectedValue] = useState('');
+    
+    let DATA
+    let USERS
+
+    useEffect(() => {
+      axios.get('http://localhost:5000/users').then(res => {
+        DATA = res.data
+        console.log(DATA)
+        USERS = DATA.map(user => {
+          return (<Picker.Item label={user.username} value={user.username} />)
+        })
+    }).catch(err => console.log(err));
+      }, []);
+
     return(
-      <View style={{flex:1, alignItems:'center', justifyContent:'center'}}>
-          <Text>Create Exercise Working!</Text>
+      <View style={{
+        flex: 1
+      }}>
+        <Text style={{fontWeight: 'bold', fontSize: 30, backgroundColor: "#008cff", padding: 15}}>Record Exercise</       Text>
+            <Text style={{padding: 15, fontSize: 20}} >Username:</Text>
+        <Picker
+          selectedValue={selectedValue}
+          style={{ height: 50, margin: 15}}
+          onValueChange={(itemValue, itemIndex) => setSelectedValue(itemValue)}
+        >
+          {USERS}
+        </Picker>
       </View>
     )
 }
--- Screens/ExerciseListScreen.js
+++ Screens/ExerciseListScreen.js
@@ -1,19 +1,10 @@
 import React, {useEffect, useState} from 'react';
-import {ActivityIndicator, FlatList, View, Text} from 'react-native';
+import {FlatList, View, Text, Pressable, StyleSheet} from 'react-native';
 import axios from 'axios';
 
 function ExerciseListScreen({ navigation }) {
 
-    
-  const [exercises, setExercises] = useState([]);
-
-//   useEffect(() => {
-//     fetch('https://reactnative.dev/movies.json')
-//       .then((response) => response.json())
-//       .then((json) => setData(json.movies))
-//       .catch((error) => console.error(error))
-//       .finally(() => setLoading(false));
-//   }, []);
+    const [exercises, setExercises] = useState([]);
 
   useEffect(() => {
     axios.get('http://localhost:5000/exercises').then(res => {
@@ -23,17 +14,62 @@ function ExerciseListScreen({ navigation }) {
   }, []);
 
   return (
-    <View style={{ flex: 1, padding: 24 }}>
-        <Text style={{fontWeight: 'bold', fontSize: 30}}>All Exercises</Text>
+    <View style={{ flex: 1}}>
+        <Text style={{fontWeight: 'bold', fontSize: 30, backgroundColor: "#008cff", padding: 15}}>All Exercises</Text>
         <FlatList
-          data={exercises}
-          keyExtractor={item => item._id}
-          renderItem={({ item }) => (
-            <Text style={{paddingTop: 15, fontSize: 20}}>{item.username}  |  {item.description}</Text>
-          )}
+            data={exercises}
+            keyExtractor={item => item._id}
+            renderItem={({ item }) => (
+                <Pressable onPress={() => {
+                    console.log('Working');
+                  }}>
+                    <View style={{flex:1, flexDirection:'row', justifyContent:'space-between', marginRight:40}}>
+                        <Text style={{padding: 15, fontSize: 20}}>{item.username}</Text>
+                        <Text style={{padding: 15, fontSize: 20}}>{item.description}</Text>
+                </View>
+                </Pressable>
+            )}
         />
     </View>
   );
 }
+const styles = StyleSheet.create({
+    centeredView: {
+      flex: 1,
+      justifyContent: "center",
+      alignItems: "center",
+      marginTop: 22
+    },
+    modalView: {
+      margin: 20,
+      backgroundColor: "white",
+      borderRadius: 20,
+      padding: 35,
+      alignItems: "center",
+      shadowColor: "#000",
+      shadowOffset: {
+        width: 0,
+        height: 2
+      },
+      shadowOpacity: 0.25,
+      shadowRadius: 3.84,
+      elevation: 5
+    },
+    openButton: {
+      backgroundColor: "#F194FF",
+      borderRadius: 20,
+      padding: 10,
+      elevation: 2
+    },
+    textStyle: {
+      color: "white",
+      fontWeight: "bold",
+      textAlign: "center"
+    },
+    modalText: {
+      marginBottom: 15,
+      textAlign: "center"
+    }
+  });
 
 export default ExerciseListScreen;
